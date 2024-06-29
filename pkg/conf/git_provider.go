462ad662ef1eabb853bc2c0469580d7bfa735282
omri assa <omriassa@gmail.com>
2024-06-29 17:55:10 +0300

    fix: done utils tests

--- pkg/conf/git_provider.go
+++ pkg/conf/git_provider.go
@@ -1,7 +1,7 @@
 package conf
 
 import (
-	"fmt"
+	"fmt" 
 
 	"github.com/kelseyhightower/envconfig"
 )
@@ -10,13 +10,13 @@ type GitProviderConfig struct {
 	Provider           string `envconfig:"GIT_PROVIDER" required:"true"`
 	Token              string `envconfig:"GIT_TOKEN" required:"true"`
 	Url			       string `envconfig:"GIT_URL" required:"true"`
-	OrgName            string `envconfig:"GIT_ORG_NAME" required:"true"`
+	OrgName           string `envconfig:"GIT_ORG_NAME" required:"true"`
 	OrgLevelWebhook    bool   `envconfig:"GIT_ORG_LEVEL_WEBHOOK" default:"false" required:"false"`
 	RepoList           string `envconfig:"GIT_WEBHOOK_REPO_LIST" required:"false"`
 	WebhookURL         string `envconfig:"GIT_WEBHOOK_URL" required:"false"`
 	WebhookSecret      string `envconfig:"GIT_WEBHOOK_SECRET" required:"false"`
 	WebhookAutoCleanup bool   `envconfig:"GIT_WEBHOOK_AUTO_CLEANUP" default:"false" required:"false"`
-  EnforceOrgBelonging bool   `envconfig:"GIT_ENFORCE_ORG_BELONGING" default:"false" required:"false"`
+    EnforceOrgBelonging bool   `envconfig:"GIT_ENFORCE_ORG_BELONGING" default:"false" required:"false"`
 	OrgID               int64
 	FullHealthCheck    bool   `envconfig:"GIT_FULL_HEALTH_CHECK" default:"false" required:"false"`
 }

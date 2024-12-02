987efb991fcc871b19b49aedba5969915434fe2c
Omri Assa <omriassa@gmail.com>
2024-12-02 13:03:21 +0200

    fix: some space

--- scripts/init-gitlab.sh
+++ scripts/init-gitlab.sh
@@ -20,6 +20,7 @@ if [ -z "$(helm list -n gitlab | grep gitlab)" ]; then
   helm upgrade --install gitlab -n gitlab gitlab/gitlab -f gitlab.values.yaml
 
   kubectl wait --namespace gitlab --for=condition=ready pod -l app=toolbox --timeout=360s
+
   GITLAB_TOOLBOX_POD=$(kubectl get pods --namespace gitlab -l app=toolbox -o name)
   TOKENS_OUTPUT=$(kubectl exec -it -c toolbox ${GITLAB_TOOLBOX_POD} -n gitlab -- gitlab-rails runner /tmp/scripts/piper-setup.rb)
   echo $TOKENS_OUTPUT
